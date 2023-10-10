7fdf3c0fc38a84bc474c7a919f6db5c82b661823
niknov80 <n.novozhilov80@gmail.com>
2023-10-10 15:44:46 +0300

    Исправлено форматирование html файлов Сделана доступность input

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -5,11 +5,11 @@ export const detectedScript = () => {
   document.querySelector('.no-js').classList.remove('no-js');
   menu.classList.add('site-nav__list--closed');
   toggle.classList.add('site-nav__toggle--open');
-}
+};
 
 export const toggleClickHandler = () => {
   toggle.addEventListener('click', () => {
     menu.classList.toggle('site-nav__list--closed');
     toggle.classList.toggle('site-nav__toggle--close');
-  })
-}
+  });
+};
